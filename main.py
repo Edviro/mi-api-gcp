7d5e48af5e3f7d0d2f1b5f2d822140473d5f6aee
Edviro <eduv0977@gmail.com>
2026-07-17 16:58:43 +0000

    feat: actualizar mensaje de bienvenida a v2

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"status": "online", "mensaje": "¡Hola Giles"}
+    return {"status": "online", "mensaje": "¡Hola versión 2.0 automatizada desde GitHub Actions! 🚀",}
 
 @app.get("/usuarios")
 def get_usuarios():
